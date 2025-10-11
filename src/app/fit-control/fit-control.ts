a75bd455531a113ebeac78bfe05412556421f2ab
Ray0711 <github_com@rhuber.de>
2025-10-11 10:12:53 +0000

    first, still useless .fit file

--- src/app/fit-control/fit-control.ts
+++ src/app/fit-control/fit-control.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FitEncoder} from "../fit-encoder";
+import {MatButton} from "@angular/material/button";
 
 @Component({
-  selector: 'app-fit-control',
-  imports: [],
-  templateUrl: './fit-control.html',
-  styleUrl: './fit-control.scss'
+    selector: 'app-fit-control',
+    imports: [
+        MatButton
+    ],
+    templateUrl: './fit-control.html',
+    styleUrl: './fit-control.scss'
 })
 export class FitControl {
 
-  download(): void {
-    console.log('Download button clicked');
-    // Add your download logic here
-  }
+    download(): void {
+        let fitEncode: FitEncoder = new FitEncoder();
+        let fitFile: Uint8Array<ArrayBufferLike> = fitEncode.encode();
+        const a = document.createElement('a');
+        const buffer = fitFile.buffer as ArrayBuffer;
+        const objectUrl = URL.createObjectURL(new Blob([buffer], {type: 'application/octet-stream'}));
+        a.href = objectUrl;
+        a.download = 'file.fit';
+        a.click();
+        URL.revokeObjectURL(objectUrl);
+        console.log('Download button clicked');
+        // Add your download logic here
+    }
 
 }
--- src/app/fit-encoder.ts
+++ src/app/fit-encoder.ts
@@ -1,12 +1,14 @@
 // Import the SDK
-import {Encoder, Profile} from "@garmin/fitsdk";
-import * as fs from "fs";
+// import {Encoder, Profile} from "@garmin/fitsdk";
+// import * as fs from "fs";
+import {Encoder, Profile, WORKOUT_MESG} from '@garmin/fitsdk';
+import {Sport, SubSport} from '../types/fitsdk_enums';
 
 export class FitEncoder {
 
-  public encode() {
+    public encode(): Uint8Array {
 // Create an Encoder
-    const encoder = new Encoder();
+        const encoder = new Encoder();
 
 //
 // Write messages to the output-stream
@@ -16,48 +18,78 @@ export class FitEncoder {
 // definitions can be found in the Profile.
 //
 
-// Pass the MesgNum and message data as separate parameters to the onMesg() method
-    encoder.onMesg(Profile.MesgNum.FILE_ID, {
-      manufacturer: "development",
-      product: 1,
-      timeCreated: new Date(),
-      type: "activity",
-    });
+// // Pass the MesgNum and message data as separate parameters to the onMesg() method
+//         encoder.onMesg(Profile.MesgNum.FILE_ID, {
+//             manufacturer: "development",
+//             product: 1,
+//             timeCreated: new Date(),
+//             type: "activity",
+//         });
 
 // The writeMesg() method expects the mesgNum to be included in the message data
 // Internally, writeMesg() calls onMesg()
-    encoder.writeMesg({
-      mesgNum: Profile.MesgNum.FILE_ID,
-      manufacturer: "development",
-      product: 1,
-      timeCreated: new Date(),
-      type: "activity",
-    });
 
-// Unknown values in the message will be ignored by the Encoder
-    encoder.onMesg(Profile.MesgNum.FILE_ID, {
-      manufacturer: "development",
-      product: 1,
-      timeCreated: new Date(),
-      type: "activity",
-      customField: 12345, // This value will be ignored by the Encoder
-    });
+        encoder.writeMesg({
+            mesgNum: Profile.MesgNum.FILE_ID,
+            manufacturer: "development",
+            product: 1,
+            timeCreated: new Date(),
+            type: "activity"
+        });
 
-// Subfield values in the message will be ignored by the Encoder
-    encoder.onMesg(Profile.MesgNum.FILE_ID, {
-      manufacturer: "development",
-      product: 4440, // This is the main product field, which is a uint16
-      garminProduct: "edge1050", // This value will be ignored by the Encoder, use the main field value instead
-      timeCreated: new Date(),
-      type: "activity",
-    });
+
+        /*
+        *   // 5. Every FIT Workout file MUST contain a Workout message as the second message
+    var workoutMesg = new WorkoutMesg();
+    workoutMesg.SetWktName("Bike Workout");
+    workoutMesg.SetSport(Sport.Cycling);
+    workoutMesg.SetSubSport(SubSport.Invalid);
+    workoutMesg.SetNumValidSteps(1);
+    encoder.Write(workoutMesg);
+
+        * */
+        encoder.writeMesg({
+            mesgNum: Profile.MesgNum.WORKOUT,
+            sport: Sport.training,
+            subSport: SubSport.strengthTraining,
+            wktName: "FMS-Test",
+            numValidSteps: 10,
+        } as WORKOUT_MESG)
+
+
+        // encoder.writeMesg({
+        //     mesgNum: Profile.MesgNum.SLAVE_DEVICE,
+        //     manufacturer: "development",
+        //     product: 1,
+        //     timeCreated: new Date(),
+        //     type: "activity"
+        // });
+
+// // Unknown values in the message will be ignored by the Encoder
+//     encoder.onMesg(Profile.MesgNum.FILE_ID, {
+//       manufacturer: "development",
+//       product: 1,
+//       timeCreated: new Date(),
+//       type: "activity",
+//       customField: 12345, // This value will be ignored by the Encoder
+//     });
+//
+// // Subfield values in the message will be ignored by the Encoder
+//     encoder.onMesg(Profile.MesgNum.FILE_ID, {
+//       manufacturer: "development",
+//       product: 4440, // This is the main product field, which is a uint16
+//       garminProduct: "edge1050", // This value will be ignored by the Encoder, use the main field value instead
+//       timeCreated: new Date(),
+//       type: "activity",
+//     });
 
 // Closing the encoder returns the file as an UInt8 Array
-    const uint8Array = encoder.close();
+        const fitFIle = encoder.close();
+        return fitFIle;
 
 // Write the file to disk,
 
-    fs.writeFileSync("example.fit", uint8Array);
-  }
+        // fs.writeFileSync("example.fit", uint8Array);
+    }
 
 }
