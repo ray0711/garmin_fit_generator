4b1f2f1d79bc9df2e22ffdbb4933aac4fd241cff
Ray0711 <github_com@rhuber.de>
2025-10-23 21:33:43 +0200

    correct exercise titles, still needs cleanup of duplicate titles next steps - implement -- notes -- repeat input -- target types (time, repetitions, ...) -- target input -- custom exercise category exerciseCategory = unknown? -- check what happens when adding moveExercises (officially not supported by epix2)

--- src/app/fit-encoder.ts
+++ src/app/fit-encoder.ts
@@ -1,6 +1,3 @@
-// import {Encoder, MappingInverter, Message_WORKOUT, Message_WORKOUT_STEP, Profile} from '@garmin/fitsdk';
-// import {FileType, MesgNum, Sport, SubSport, WktStepDuration} from '../types/fitsdk_enums';
-// import {Profile} from '../types_generated';
 import Encoder from '../types/encoder';
 import {
   ExerciseCategory,
@@ -10,20 +7,14 @@ import {
   Message_WORKOUT,
   Message_WORKOUT_STEP,
 } from '../types/MessageTypes';
-// import Sport = Profile.types.subSport;
-// import {Profile} from '../types_generated';
-// import {Encoder, Profile} from '../types/generated';
 import {FileType, Manufacturer, MesgNum, Sport, SubSport, WktStepDuration, WktStepTarget} from '../types/fitsdk_enums';
 import {Block, RepeatBlock} from './workout-builder/block';
 import {Profile} from '../types_generated';
-// import Encoder from '../types/encoder';
-// import {Message_WORKOUT_STEP} from '../types/MessageTypes';
-// import {Profile} from '../types_generated';
-// import MesgNum = ProfileTypes;
-// import '../types/ProfileTypes';
-// import MesgNum = Profile.MesgNum;
-// import MesgNum = Profile.MesgNum;
 
+interface WORKOUT_STEP_AND_TITLE {
+  workoutStep: Message_WORKOUT_STEP,
+  exerciseTitle: Message_EXERCISE_TITLE | undefined,
+}
 
 export class FitEncoder {
   getFileMessage(): Message_FILE_ID {
@@ -84,28 +75,34 @@ export class FitEncoder {
     return entry[1] as T;
   }
 
-  getWorkoutStepMessage(block: Block, messageIndex: number): Message_WORKOUT_STEP[] {
+  getWorkoutStepMessage(block: Block, messageIndex: number): WORKOUT_STEP_AND_TITLE[] {
     if (block instanceof RepeatBlock) {
       return [
-        ...block.children.flatMap((child) => this.getWorkoutStepMessage(child, messageIndex )),
-        this.getRepeatMessage(block),
+        ...block.children.flatMap((child) => this.getWorkoutStepMessage(child, messageIndex)),
+        {workoutStep: this.getRepeatMessage(block), exerciseTitle: undefined}
       ];
     } else {
       const exerciseCategory = this.findEnumValue(ExerciseCategory, block.categoryGarmin) as ExerciseCategory;
-      return [{
+      const exerciseName = this.findExeriseNumber(exerciseCategory, block.nameGarmin)
+      const workoutStepMessage: Message_WORKOUT_STEP = {
         mesgNum: MesgNum.WORKOUT_STEP,
-        messageIndex: messageIndex ,
-        exerciseName: this.findExeriseNumber(exerciseCategory, block.nameGarmin),
-        exerciseCategory: this.findEnumValue(ExerciseCategory, block.categoryGarmin),
-        durationValue: 20,
+        messageIndex: messageIndex,
+        exerciseName: exerciseName,
+        exerciseCategory: exerciseCategory,
+        durationValue: 60000,
         durationType: WktStepDuration.time,
         targetType: WktStepTarget.open,
         targetValue: 0,
         secondaryTargetValue: 0,
         notes: 'some notes',
         wktStepName: block.name,
-
-      } as Message_WORKOUT_STEP];
+      };
+      const exerciseTitleMessage: Message_EXERCISE_TITLE = this.getExerciseTitleMessage(
+        exerciseName,
+        exerciseCategory,
+        block.name,
+      );
+      return [{workoutStep: workoutStepMessage, exerciseTitle: exerciseTitleMessage}];
     }
   }
 
@@ -145,10 +142,9 @@ export class FitEncoder {
     } as Message_FILE_CREATOR;
   }
 
-  getExerciseTitleMessage(exerciseName: number, exerciseCategory: ExerciseCategory, exerciseTitle: string, messageIndex: number): Message_EXERCISE_TITLE {
+  getExerciseTitleMessage(exerciseName: number, exerciseCategory: ExerciseCategory, exerciseTitle: string): Message_EXERCISE_TITLE {
     return {
       mesgNum: MesgNum.EXERCISE_TITLE,
-      messageIndex: messageIndex,
       exerciseName: exerciseName,
       exerciseCategory: exerciseCategory,
       wktStepName: exerciseTitle,
@@ -158,86 +154,38 @@ export class FitEncoder {
 
   public encode(workout: Block[]): Uint8Array {
     const encoder = new Encoder();
-    let workoutMessages: Message_WORKOUT_STEP[] = [];
+    let workoutstepandtitles: WORKOUT_STEP_AND_TITLE[] = [];
 
     encoder.writeMesg(this.getFileMessage());
     encoder.writeMesg(this.getFileCreatorMessage());
 
     let messageIndex = 0;
     for (const block of workout) {
-      workoutMessages = workoutMessages.concat(this.getWorkoutStepMessage(block, messageIndex));
+      workoutstepandtitles = workoutstepandtitles.concat(this.getWorkoutStepMessage(block, messageIndex));
       messageIndex += 1;
     }
 
     encoder.writeMesg(this.getWorkoutMessage(
-      workoutMessages.length,
+      workoutstepandtitles.length,
       Sport.training,
       SubSport.strengthTraining,
-      'test workout',
-      'test workout description',
+      'test ' + new Date().toISOString(),
+      'description: ' + new Date().toISOString(),
     ));
 
-    for (const message of workoutMessages) {
-      encoder.writeMesg(message);
+    for (const workoutstepandtitle of workoutstepandtitles) {
+      encoder.writeMesg(workoutstepandtitle.workoutStep);
     }
 
-    let exerciseTitleMessagesIndex= 0;
-    for (const msg of workoutMessages) {
-      encoder.writeMesg(this.getExerciseTitleMessage(msg.exerciseName as number, msg.exerciseCategory as ExerciseCategory, msg.notes as string, exerciseTitleMessagesIndex));
-      exerciseTitleMessagesIndex += 1;
+    let exerciseTitleMessagesIndex = 0;
+    for (const msg of workoutstepandtitles) {
+      if (msg.exerciseTitle) {
+        msg.exerciseTitle.messageIndex = exerciseTitleMessagesIndex;
+        encoder.writeMesg(msg.exerciseTitle);
+        exerciseTitleMessagesIndex += 1;
+      }
     }
-
 
     return encoder.close();
-    //
-    // Write messages to the output-stream
-    //
-    // The message data should match the format returned by
-    // the Decoder. Field names should be camelCase. The fields
-    // definitions can be found in the Profile.
-    //
-
-    // // Pass the MesgNum and message data as separate parameters to the onMesg() method
-    //         encoder.onMesg(MesgNum.FILE_ID, {
-    //             manufacturer: "development",
-    //             product: 1,
-    //             timeCreated: new Date(),
-    //             type: "activity",
-    //         });
-
-    // The writeMesg() method expects the mesgNum to be included in the message data
-    // Internally, writeMesg() calls onMesg()
-
-
-    /*
-    *   // 5. Every FIT Workout file MUST contain a Workout message as the second message
-var workoutMesg = new WorkoutMesg();
-workoutMesg.SetWktName("Bike Workout");
-workoutMesg.SetSport(Sport.Cycling);
-workoutMesg.SetSubSport(SubSport.Invalid);
-workoutMesg.SetNumValidSteps(1);
-encoder.Write(workoutMesg);
-
-        * */
-    /*    encoder.writeMesg({
-          mesgNum: MesgNum.WORKOUT,
-          sport: Sport.training,
-          subSport: SubSport.strengthTraining,
-          wktName: 'FMS-Test',
-          numValidSteps: 10,
-          wktDescription: 'Test Workout',
-        });
-
-        for (let i = 0; i < 10; i++) {
-          const step: Message_WORKOUT_STEP = {
-            mesgNum: MesgNum.WORKOUT_STEP,
-            durationType: WktStepDuration.time,
-            durationValue: 10,
-            exerciseCategory: 0,
-            exerciseName: 0,
-            notes: '',
-            wktStepName: '',
-          };
-        }*/
   }
 }
